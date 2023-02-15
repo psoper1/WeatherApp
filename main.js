46c53d2a832f2b04be44a4ca79f70ff4d430c69c
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-15 23:35:41 +0000

    made a small framework for mvp

--- main.js
+++ main.js
@@ -3,13 +3,72 @@ let apiKey = "82d10d8b9124f9ece3e56c62befeda72";
 
 
 
-function placeHolder() {
+// function placeHolder() {
     let main = document.getElementById("main");
     let theStrap = document.createElement("div");
-    // let bootstrap = document.getElementById("bootstrap");
+    theStrap.className = "mainStrap";
+    main.appendChild(theStrap);
+
+    let aboveButton = document.createElement("div");
+    let text2 = document.createElement("h2");
+    text2.textContent = "What's the weather like today? Put in your ZIP and find out!";
+    text2.className = "font-bold text-danger";
+    aboveButton.appendChild(text2);
+    theStrap.appendChild(aboveButton);
+
+    let section = document.createElement("div");
+    section.className = "flex flex-column";
+    let btn = document.createElement("button");
+    btn.textContent = "Submit";
+    section.appendChild(btn);
+    let btn2 = document.createElement("input");
+    section.appendChild(btn2);
+    theStrap.appendChild(section);
+
+
+    let cityInfo = document.createElement("div");
+    let text = document.createElement("p");
+    text.textContent = "City:";
+    text.className = "font-bold text-danger";
+    text.id = "city";
+    cityInfo.appendChild(text);
+    theStrap.appendChild(cityInfo);
+
+    let setTemp = document.createElement("div");
+    let text3 = document.createElement("p");
+    text3.textContent = "Temperature:";
+    text3.className = "font-bold text-danger";
+    text3.id = "temp";
+    setTemp.appendChild(text3);
+    theStrap.appendChild(setTemp);
+
+    let setCondition = document.createElement("div");
+    let text4 = document.createElement("p");
+    text4.textContent = "Conditions:";
+    text4.className = "font-bold text-danger";
+    text4.id = "cond";
+    setCondition.appendChild(text4);
+    theStrap.appendChild(setCondition);
+
+// }
+
+// placeHolder();
+
+
+
+
+
+
+
+
+
+
+
+
+// let bootstrap = document.getElementById("bootstrap");
     // let row = document.createElement("div");
     // let column = document.createElement("div");
-    main.appendChild(theStrap);
+    // main.appendChild(theStrap);
     // theStrap.appendChild(row);
     // row.appendChild(column);
     // theStrap.setAttribute("id", "bootstrap");
@@ -22,39 +81,6 @@ function placeHolder() {
     // btn.setAttribute("id", "onButton");
     // btn.appendChild(document.createTextNode('Submit'));
     // section.appendChild(btn);
-
-    let section = document.createElement("div");
-    section.className = "flex flex-column";
-    let btn = document.createElement("button");
-    btn.textContent = "Submit";
-    section.appendChild(btn);
-    let btn2 = document.createElement("input");
-    section.appendChild(btn2);
-    theStrap.appendChild(section);
-
-    let middleSection = document.createElement("div");
-    let text = document.createElement("p");
-    text.textContent = "Hello";
-    text.className = "font-bold text-danger";
-    middleSection.appendChild(text);
-    theStrap.appendChild(middleSection);
-    
-}
-
-placeHolder();
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
