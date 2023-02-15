ada7ddeba2601356cc2cca0a93457453fabdae7e
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-15 21:31:02 +0000

    working it out

--- main.js
+++ main.js
@@ -7,24 +7,45 @@ function placeHolder() {
     let main = document.getElementById("main");
     let theStrap = document.createElement("div");
     // let bootstrap = document.getElementById("bootstrap");
-    let row = document.createElement("div");
-    let column = document.createElement("div");
+    // let row = document.createElement("div");
+    // let column = document.createElement("div");
     main.appendChild(theStrap);
-    theStrap.appendChild(row);
-    row.appendChild(column);
-    theStrap.setAttribute("id", "bootstrap");
-    theStrap.classList.add("container");
-    row.classList.add("row");
-    column.classList.add("text-danger");
-    // row.appendChild(column.cloneNode());
-    column.innerText = "test3";
+    // theStrap.appendChild(row);
+    // row.appendChild(column);
+    // theStrap.setAttribute("id", "bootstrap");
+    // theStrap.classList.add("container");
+    // row.classList.add("row");
+    // column.innerText = "test3";
+    // column.className = "text-success";
+    // let btn = document.createElement("button");
+    // btn.textContent = "Submit";
+    // btn.setAttribute("id", "onButton");
+    // btn.appendChild(document.createTextNode('Submit'));
+    // section.appendChild(btn);
+
+    let section = document.createElement("div");
+    section.className = "flex flex-column";
     let btn = document.createElement("button");
-    btn.setAttribute("id", "onButton");
-    main.appendChild(btn);
-    btn.innerText("Test");
+    btn.textContent = "Submit";
+    section.appendChild(btn);
+    let btn2 = document.createElement("input");
+    section.appendChild(btn2);
+    theStrap.appendChild(section);
+
+    let middleSection = document.createElement("div");
+    let text = document.createElement("p");
+    text.textContent = "Hello";
+    text.className = "font-bold text-danger";
+    middleSection.appendChild(text);
+    theStrap.appendChild(middleSection);
+    
 }
 
 placeHolder();
+
+
+
+
 
 
 
