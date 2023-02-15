bc0aad6d87739a339ab4ea1bbea22abda2880767
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-15 20:22:36 +0000

    something

--- main.js
+++ main.js
@@ -1,7 +1,120 @@
 let url = "https://api.openweathermap.org/";
 let apiKey = "82d10d8b9124f9ece3e56c62befeda72";
-let main = document.getElementById("main");
 
-main.onload = function () {
 
+
+function placeHolder() {
+    let main = document.getElementById("main");
+    let theStrap = document.createElement("div");
+    // let bootstrap = document.getElementById("bootstrap");
+    let row = document.createElement("div");
+    let column = document.createElement("div");
+    main.appendChild(theStrap);
+    theStrap.appendChild(row);
+    row.appendChild(column);
+    theStrap.setAttribute("id", "bootstrap");
+    theStrap.classList.add("container");
+    row.classList.add("row");
+    column.classList.add("text-danger");
+    // row.appendChild(column.cloneNode());
+    column.innerText = "test3";
+    let btn = document.createElement("button");
+    btn.setAttribute("id", "onButton");
+    main.appendChild(btn);
+    btn.innerText("Test");
 }
+
+placeHolder();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create variable for the main div
+// let main = document.getElementById("main");
+//create a button element using createElement
+// let btn = document.createElement("button");
+// btn.setAttribute("id", "onButton");
+// main.appendChild(btn);
+//Set the ID to onButton using setAttribute so I can call to it later
+// btn.setAttribute("id", "onButton");
+
+
+
+
+
+
+// main.innerText = "What's the weather like? \n Type in your ZIP code to find out!";
+
+
+
+
+
+
+// let main = document.getElementById("main");
+// let theStrap = document.createElement("div");
+// // let bootstrap = document.getElementById("bootstrap");
+// let row = document.createElement("div");
+// let column = document.createElement("div");
+// main.appendChild(theStrap);
+// theStrap.appendChild(row);
+// row.appendChild(column);
+// theStrap.setAttribute("id", "bootstrap");
+// theStrap.classList.add("container");
+// row.classList.add("row");
+// column.classList.add("col-sm");
+
+
+// theStrap.innerText = "test";
+// row.innerText = "test2";
+// column.innerText = "test3";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let theStrap = document.createElement("div");
+// let bootstrap = document.getElementById("bootstrap");
+// let row = document.createElement("div");
+// let column = document.createElement("div");
+// main.appendChild(theStrap);
+// theStrap.appendChild(row);
+// row.appendChild(column);
+// theStrap.setAttribute = ("id", "bootstrap");
+// theStrap.classList.add("container");
+// row.classList.add("row");
+// column.classList.add("col-sm");
+
+
+// theStrap.innerText = "test";
+// row.innerText = "test2";
+// column.innerText = "test3";
