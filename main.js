55486dd5160d03adb7ef70cd86f5ee992a0ab2c9
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-17 01:52:57 +0000

    mostly finished

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let main = document.getElementById("main");
+main.className = "center";
 let theStrap = document.createElement("div");
 theStrap.className = "mainStrap";
 main.appendChild(theStrap);
@@ -6,26 +7,54 @@ main.appendChild(theStrap);
 let aboveButton = document.createElement("div");
 let text2 = document.createElement("h3");
 text2.textContent = "What's the weather like today? Put in your ZIP and find out!";
-text2.className = "font-bold";
+text2.className = "d-flex justify-content-center";
 aboveButton.appendChild(text2);
 theStrap.appendChild(aboveButton);
 
 let section = document.createElement("div");
-section.className = "flex flex-column";
-let btn = document.createElement("button");
-btn.textContent = "Submit";
-section.appendChild(btn);
-btn.id = "inputValue";
+section.className = "d-flex justify-content-center";
 let btn2 = document.createElement("input");
 btn2.id = "inputField";
 section.appendChild(btn2);
 theStrap.appendChild(section);
+let btn = document.createElement("button");
+btn.textContent = "Submit";
+section.appendChild(btn);
+btn.id = "inputValue";
+btn.className = "btn btn-outline-primary";
+
+// let uList = document.createElement("ul");
+// uList.className = "list-group";
+// let liOne = document.createElement("li");
+// liOne.className = "list-group-item";
+// liOne.id = "listOneCityString";
+// uList.appendChild(liOne);
+// let liTwo = document.createElement("li");
+// liTwo.className = "list-group-item";
+// liTwo.id = "listOneCityResult";
+// uList.appendChild(liTwo);
+// let liThree = document.createElement("li");
+// liThree.className = "list-group-item";
+// liThree.id = "listTwoTempString";
+// uList.appendChild(liThree);
+// let liFour = document.createElement("li");
+// liFour.className = "list-group-item";
+// liFour.id = "listTwoTempResult";
+// uList.appendChild(liFour);
+// let liFive = document.createElement("li");
+// liFive.className = "list-group-item";
+// liFive.id = "listThreeCondString";
+// uList.appendChild(liFive);
+// let liSix = document.createElement("li");
+// liSix.className = "list-group-item";
+// liSix.is = "listThreeCondResult";
+// uList.appendChild(liSix);
 
 
 let cityInfo = document.createElement("div");
 let text = document.createElement("p");
 text.textContent = "City:";
-text.className = "font-bold text-success";
+text.className = "h4 d-flex justify-content-center font-bold text-success";
 text.id = "city";
 cityInfo.appendChild(text);
 theStrap.appendChild(cityInfo);
@@ -33,7 +62,7 @@ theStrap.appendChild(cityInfo);
 let setTemp = document.createElement("div");
 let text3 = document.createElement("p");
 text3.textContent = "Temperature:";
-text3.className = "font-bold text-success";
+text3.className = "h4 d-flex justify-content-center font-bold text-success";
 text3.id = "temp";
 setTemp.appendChild(text3);
 theStrap.appendChild(setTemp);
@@ -41,33 +70,46 @@ theStrap.appendChild(setTemp);
 let setCondition = document.createElement("div");
 let text4 = document.createElement("p");
 text4.textContent = "Conditions:";
-text4.className = "font-bold text-success";
+text4.className = "h4 d-flex justify-content-center font-bold text-success";
 text4.id = "cond";
 setCondition.appendChild(text4);
 theStrap.appendChild(setCondition);
 
-btn.addEventListener("click", function () {
+
+
+btn.addEventListener("click", function getAPI() {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${btn2.value}&appid=82d10d8b9124f9ece3e56c62befeda72`)
         .then(response => {
-            console.log(response.data);
+
             cityData = response.data.name;
             temperatureK = Math.round(response.data.main.temp * 9 / 5 - 459.67);
 
             conditionsW = response.data.weather[0].description;
-            console.log(response.cityData);
 
+            // Setting conditionW manually to show off if statement
+            // conditionsW = "overcast clouds";
+            // conditionsW = "broken clouds"
+
+            text.innerText = "City: " + cityData;
+            text3.innerText = "It is " + temperatureK + "F outside";
+            text4.innerText = "Forecast is: " + conditionsW;
+            let imgs = document.createElement("img");
+            imgs.id = "img";
+            theStrap.appendChild(imgs);
+            imgs.src = "";
+            imgs.className = "rounded mx-auto d-block";
+
+            if (conditionsW === "overcast clouds") {
+                imgs.src = "cloud.png";
+            }
+            else if (conditionsW === "broken clouds") {
+                imgs.src = "clouds.png";
+            }
             console.log(cityData);
             console.log(temperatureK);
             console.log(conditionsW);
-
-
-
-            text.innerText = cityData;
-            text3.innerText = temperatureK;
-            text4.innerText = conditionsW;
-
         })
-        .catch(error => alert("Please enter a valid Zip Code", error));
+        .catch(error => alert("Looks like you picked yourself a bouquet of Oopsie Daisies and didn't enter a valid ZIP Code!", error));
 })
 
 
