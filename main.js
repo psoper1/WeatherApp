66ea6a1cef88b3611b0c0b8a371df276b1594663
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-17 01:56:03 +0000

    made a messing around js file

--- main.js
+++ main.js
@@ -23,33 +23,6 @@ section.appendChild(btn);
 btn.id = "inputValue";
 btn.className = "btn btn-outline-primary";
 
-// let uList = document.createElement("ul");
-// uList.className = "list-group";
-// let liOne = document.createElement("li");
-// liOne.className = "list-group-item";
-// liOne.id = "listOneCityString";
-// uList.appendChild(liOne);
-// let liTwo = document.createElement("li");
-// liTwo.className = "list-group-item";
-// liTwo.id = "listOneCityResult";
-// uList.appendChild(liTwo);
-// let liThree = document.createElement("li");
-// liThree.className = "list-group-item";
-// liThree.id = "listTwoTempString";
-// uList.appendChild(liThree);
-// let liFour = document.createElement("li");
-// liFour.className = "list-group-item";
-// liFour.id = "listTwoTempResult";
-// uList.appendChild(liFour);
-// let liFive = document.createElement("li");
-// liFive.className = "list-group-item";
-// liFive.id = "listThreeCondString";
-// uList.appendChild(liFive);
-// let liSix = document.createElement("li");
-// liSix.className = "list-group-item";
-// liSix.is = "listThreeCondResult";
-// uList.appendChild(liSix);
-
 
 let cityInfo = document.createElement("div");
 let text = document.createElement("p");
@@ -74,7 +47,6 @@ text4.className = "h4 d-flex justify-content-center font-bold text-success";
 text4.id = "cond";
 setCondition.appendChild(text4);
 theStrap.appendChild(setCondition);
-
 
 
 btn.addEventListener("click", function getAPI() {
@@ -111,129 +83,3 @@ btn.addEventListener("click", function getAPI() {
         })
         .catch(error => alert("Looks like you picked yourself a bouquet of Oopsie Daisies and didn't enter a valid ZIP Code!", error));
 })
-
-
-
-
-
-
-
-
-
-
-
-// Code that didn't work but got me to a finished"ish" product
-
-// let url = "https://api.openweathermap.org/";
-// let apiKey = "82d10d8b9124f9ece3e56c62befeda72";
-
-
-
-// function placeHolder() {
-
-
-    // }
-
-// placeHolder();
-
-
-
-
-
-
-
-
-// (temperatureK * 9) / 5 - 459.67;
-
-
-
-// let bootstrap = document.getElementById("bootstrap");
-    // let row = document.createElement("div");
-    // let column = document.createElement("div");
-    // main.appendChild(theStrap);
-    // theStrap.appendChild(row);
-    // row.appendChild(column);
-    // theStrap.setAttribute("id", "bootstrap");
-    // theStrap.classList.add("container");
-    // row.classList.add("row");
-    // column.innerText = "test3";
-    // column.className = "text-success";
-    // let btn = document.createElement("button");
-    // btn.textContent = "Submit";
-    // btn.setAttribute("id", "onButton");
-    // btn.appendChild(document.createTextNode('Submit'));
-    // section.appendChild(btn);
-
-
-
-
-
-
-
-//create variable for the main div
-// let main = document.getElementById("main");
-//create a button element using createElement
-// let btn = document.createElement("button");
-// btn.setAttribute("id", "onButton");
-// main.appendChild(btn);
-//Set the ID to onButton using setAttribute so I can call to it later
-// btn.setAttribute("id", "onButton");
-
-
-
-
-
-
-// main.innerText = "What's the weather like? \n Type in your ZIP code to find out!";
-
-
-
-
-
-
-// let main = document.getElementById("main");
-// let theStrap = document.createElement("div");
-// // let bootstrap = document.getElementById("bootstrap");
-// let row = document.createElement("div");
-// let column = document.createElement("div");
-// main.appendChild(theStrap);
-// theStrap.appendChild(row);
-// row.appendChild(column);
-// theStrap.setAttribute("id", "bootstrap");
-// theStrap.classList.add("container");
-// row.classList.add("row");
-// column.classList.add("col-sm");
-
-
-// theStrap.innerText = "test";
-// row.innerText = "test2";
-// column.innerText = "test3";
-
-
-
-
-// City id is city
-// Temperature id is temp
-// Conditions id is cond
-
-// let cityData = "";
-// let temperatureF = "";
-// let conditionsW = "";
-
-
-// let theStrap = document.createElement("div");
-// let bootstrap = document.getElementById("bootstrap");
-// let row = document.createElement("div");
-// let column = document.createElement("div");
-// main.appendChild(theStrap);
-// theStrap.appendChild(row);
-// row.appendChild(column);
-// theStrap.setAttribute = ("id", "bootstrap");
-// theStrap.classList.add("container");
-// row.classList.add("row");
-// column.classList.add("col-sm");
-
-
-// theStrap.innerText = "test";
-// row.innerText = "test2";
-// column.innerText = "test3";
---
+++ messingaround.js
@@ -0,0 +1,140 @@
+// let url = "https://api.openweathermap.org/";
+// let apiKey = "82d10d8b9124f9ece3e56c62befeda72";
+
+
+
+// function placeHolder() {
+
+
+    // }
+
+// placeHolder();
+
+
+
+
+
+
+
+
+// (temperatureK * 9) / 5 - 459.67;
+
+
+
+// let bootstrap = document.getElementById("bootstrap");
+    // let row = document.createElement("div");
+    // let column = document.createElement("div");
+    // main.appendChild(theStrap);
+    // theStrap.appendChild(row);
+    // row.appendChild(column);
+    // theStrap.setAttribute("id", "bootstrap");
+    // theStrap.classList.add("container");
+    // row.classList.add("row");
+    // column.innerText = "test3";
+    // column.className = "text-success";
+    // let btn = document.createElement("button");
+    // btn.textContent = "Submit";
+    // btn.setAttribute("id", "onButton");
+    // btn.appendChild(document.createTextNode('Submit'));
+    // section.appendChild(btn);
+
+
+
+
+
+
+
+//create variable for the main div
+// let main = document.getElementById("main");
+//create a button element using createElement
+// let btn = document.createElement("button");
+// btn.setAttribute("id", "onButton");
+// main.appendChild(btn);
+//Set the ID to onButton using setAttribute so I can call to it later
+// btn.setAttribute("id", "onButton");
+
+
+
+
+
+
+// main.innerText = "What's the weather like? \n Type in your ZIP code to find out!";
+
+
+
+
+
+
+// let main = document.getElementById("main");
+// let theStrap = document.createElement("div");
+// // let bootstrap = document.getElementById("bootstrap");
+// let row = document.createElement("div");
+// let column = document.createElement("div");
+// main.appendChild(theStrap);
+// theStrap.appendChild(row);
+// row.appendChild(column);
+// theStrap.setAttribute("id", "bootstrap");
+// theStrap.classList.add("container");
+// row.classList.add("row");
+// column.classList.add("col-sm");
+
+
+// theStrap.innerText = "test";
+// row.innerText = "test2";
+// column.innerText = "test3";
+
+
+
+
+// City id is city
+// Temperature id is temp
+// Conditions id is cond
+
+// let cityData = "";
+// let temperatureF = "";
+// let conditionsW = "";
+
+
+// let theStrap = document.createElement("div");
+// let bootstrap = document.getElementById("bootstrap");
+// let row = document.createElement("div");
+// let column = document.createElement("div");
+// main.appendChild(theStrap);
+// theStrap.appendChild(row);
+// row.appendChild(column);
+// theStrap.setAttribute = ("id", "bootstrap");
+// theStrap.classList.add("container");
+// row.classList.add("row");
+// column.classList.add("col-sm");
+
+
+// theStrap.innerText = "test";
+// row.innerText = "test2";
+// column.innerText = "test3";
+
+// let uList = document.createElement("ul");
+// uList.className = "list-group";
+// let liOne = document.createElement("li");
+// liOne.className = "list-group-item";
+// liOne.id = "listOneCityString";
+// uList.appendChild(liOne);
+// let liTwo = document.createElement("li");
+// liTwo.className = "list-group-item";
+// liTwo.id = "listOneCityResult";
+// uList.appendChild(liTwo);
+// let liThree = document.createElement("li");
+// liThree.className = "list-group-item";
+// liThree.id = "listTwoTempString";
+// uList.appendChild(liThree);
+// let liFour = document.createElement("li");
+// liFour.className = "list-group-item";
+// liFour.id = "listTwoTempResult";
+// uList.appendChild(liFour);
+// let liFive = document.createElement("li");
+// liFive.className = "list-group-item";
+// liFive.id = "listThreeCondString";
+// uList.appendChild(liFive);
+// let liSix = document.createElement("li");
+// liSix.className = "list-group-item";
+// liSix.is = "listThreeCondResult";
+// uList.appendChild(liSix);
